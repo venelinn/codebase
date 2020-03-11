a21a5dd8cb0709e35debef592a173aef0febfe49
Venelin Nikolov <venelinn@gmail.com>
2020-03-11 19:12:45 -0400

    detect escape

---
+++ escapeHooks.js
@@ -0,0 +1,14 @@
+const escFunction = useCallback((event) => {
+    if(event.keyCode === 27) {
+      ');
+    }
+  }, []);
+
+
+  useEffect(() => {
+    document.addEventListener("keydown", escFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    };
+  }, []);
